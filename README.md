16e1dfe3f779d1e6db4f95055e0a07e3416faab6
alvkeke <alvkeke@126.com>
2021-08-15 14:18:09 +0800

    [update] add command line parameters.

--- README.md
+++ README.md
@@ -4,4 +4,15 @@
 
 本程序是应用在Linux的[文件传输工具](https://github.com/alvkeke/LAN-File-Transfer-Java )的命令行控制前端，本程序所使用的协议在该仓库中已经进行了说明，此处不再进行赘述，如有需要请自行查阅[文档](https://github.com/alvkeke/LAN-File-Transfer-Java/blob/master/README.md#控制协议 )。
 
+## 使用方法
 
+```shell
+lft-ctrl [-p port] [-d ip] [cmd]
+```
+
+**目前只有raw模式可用**
+
+### 命令行参数
+* `-p` : 指定控制端口,默认10001
+* `-d` : 指定设备IP,默认为本机(127.0.0.1)
+* `cmd` : 要运行的命令, 若留空则进入repl
--- main.c
+++ main.c
@@ -1,19 +1,99 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 #include <malloc.h>
 #include <string.h>
 #include "ctrl.h"
 #include "repl_loop.h"
 
+
+enum CmdState{
+    NONE,
+    IPADDRESS,
+    PORT,
+};
+
+int parsePort(char *str)
+{
+    int rst = 0;
+    for (int i = 0; i < strlen(str); ++i)
+    {
+        if (str[i] < '0' || str[i] >'9') return -1;
+        rst*=10;
+        rst+= str[i]-'0';
+    }
+
+    return rst;
+}
+
 int main(int argc, char** argv)
 {
 
-    if (argc == 1)
+    int cmd_state = NONE;
+    int port = -1;
+    int i_sip = -1;
+    int i_cmd = -1;
+
+    for (int i = 1; i < argc; ++i)
     {
-//        main_loop_raw("192.168.1.10", 10001);
-        main_loop_raw("127.0.0.1", 15000);
-        return 0;
+        if (cmd_state == IPADDRESS)
+        {
+            if (i_sip >= 0) continue;
+            i_sip = i;
+        }
+        else if (PORT == cmd_state)
+        {
+            if (port > 0) continue;
+            port = parsePort(argv[i]);
+        }
+
+        if (cmd_state != NONE)
+        {
+            cmd_state = NONE;
+            continue;
+        }
+
+        if (strcmp("-d", argv[i]) == 0)
+        {
+            cmd_state = IPADDRESS;
+        }
+        else if (strcmp("-p", argv[i]) == 0)
+        {
+            cmd_state = PORT;
+        }
+        else
+        {
+            if (i_cmd >= 0) continue;
+            i_cmd = i;
+        }
     }
 
+    if (port <0) port = 10001;
+    char *sip;
+    if (i_sip<0)
+        sip = "127.0.0.1";
+    else
+        sip = argv[i_sip];
+
+
+    if (i_cmd<0)
+        main_loop_raw(sip, port);
+    else
+    {
+
+        if (strcmp(argv[i_cmd], "exit") == 0) return 0;
+        int ret = ctrl_conn_create(sip, port);
+        if (ret) {
+            printf("ERROR: cannot create connection: %d\n", ret);
+            return ret;
+        }
+
+        ctrl_exec_raw(argv[i_cmd], 1);
+
+        ctrl_conn_close();
+    }
 
     return 0;
+
+
 }
